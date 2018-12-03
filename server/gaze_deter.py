31324e0fac2743109b1a760262adafe7d22e1146
shiroshanshan <scufyx2013@yahoo.co.jp>
2018-12-03 12:44:15 +0900

    create roadmap and fix bugs

--- server/gaze_deter.py
+++ server/gaze_deter.py
@@ -1,2 +1,13 @@
+import numpy as np
+
 def gaze_determinator(gaze_direction):
-    return 0
+
+    left = gaze_direction['left_eye']
+    right = gaze_direction['right_eye']
+    vertical_vector = np.array([0, 0, -1])
+    left = np.sum(left * vertical)
+    right = np.sum(right * vertical)
+    if left + right < 2 * np.cos(np.pi/9):
+        return True
+    else:
+        return False
